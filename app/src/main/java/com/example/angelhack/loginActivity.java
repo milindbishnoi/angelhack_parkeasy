1e09cb7ac87dd2be5cbf7cdaa9227c7a44d5a8fc
hitensoni <hitensoni14@gmail.com>
2019-05-10 01:10:35 +0530

    icon updated, otp modified ready for database and maps

--- app/src/main/java/com/example/angelhack/loginActivity.java
+++ app/src/main/java/com/example/angelhack/loginActivity.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,48 +21,57 @@ import com.google.firebase.auth.SignInMethodQueryResult;
 public class loginActivity extends AppCompatActivity {
     EditText etUser, etPass;
     Button btnSignin;
-    FirebaseAuth firebaseAuth;
-    ProgressDialog progressDialog;
+    TextView tvPrompt;
+    FirebaseAuth firebaseAuth1;
+    ProgressDialog progressDialog1;
+    static boolean check1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         etUser = findViewById(R.id.etUser);
         etPass = findViewById(R.id.etPass);
+        tvPrompt = findViewById(R.id.tvPrompt);
         btnSignin = findViewById(R.id.btnSignin);
-        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog1 = new ProgressDialog(this);
+        firebaseAuth1 = FirebaseAuth.getInstance();
     }
-    public void loginUser(View view){
-        String user, password;
+    public void loginUser(View view) {
+        String user, pass;
         user = etUser.getText().toString().trim();
-        password = etPass.getText().toString().trim();
-        firebaseAuth.fetchSignInMethodsForEmail(user).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
-            @Override
-            public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
-                boolean check = task.getResult().getSignInMethods().isEmpty();
-                if(check)
-                    Toast.makeText(getApplicationContext(),"Email doest not match with out records",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        if(TextUtils.isEmpty(user)||TextUtils.isEmpty(password))
-                Toast.makeText(getApplicationContext(),"Please enter valid inputs",Toast.LENGTH_SHORT).show();
-        else {
-            progressDialog = new ProgressDialog(this);
-            progressDialog.setTitle("Sign-in");
-            progressDialog.setMessage("Please wait while we get you logged in...");
-            progressDialog.show();
-            firebaseAuth.signInWithEmailAndPassword(user,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        pass = etPass.getText().toString().trim();
+        if (TextUtils.isEmpty(user) || TextUtils.isEmpty(pass) || TextUtils.isEmpty(user))
+            Toast.makeText(this, "Please enter valid inputs", Toast.LENGTH_SHORT).show();
+        else if (true) {
+            firebaseAuth1.fetchSignInMethodsForEmail(user).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
                 @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    progressDialog.dismiss();
-                    if(task.isSuccessful()){
-                        Toast.makeText(getApplicationContext(),"Login successful",Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(),profile_activity.class);
-                        startActivity(intent);
+                public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                    check1 = task.getResult().getSignInMethods().isEmpty();
+                    if (check1) {
+                        Toast.makeText(loginActivity.this, "E-Mail address not found in our records", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
+            progressDialog1.setTitle("Loggin in");
+            progressDialog1.setMessage("Please wait while we get a few things done...");
+            progressDialog1.show();
+            firebaseAuth1.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    progressDialog1.dismiss();
+                    if(task.isSuccessful()){
+                        Toast.makeText(loginActivity.this,"Login successful",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(loginActivity.this,com.example.angelhack.profile_activity.class);
+                        startActivity(intent);
+                    }
+                    else
+                        Toast.makeText(loginActivity.this,"An error occurred. Please try again later.",Toast.LENGTH_SHORT).show();
+                }
+            });
         }
+    }
+    public void prompt(View view){
+        Intent intent = new Intent(loginActivity.this,com.example.angelhack.registerActivity.class);
+        startActivity(intent);
     }
 }
